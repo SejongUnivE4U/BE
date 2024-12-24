094d9350e6fda3a51484839e78cb37313bb929b2
Kim Haerin <108577676+jenny1zzang@users.noreply.github.com>
2024-12-24 19:08:14 +0900

    Revert "추가 개발"

--- src/main/java/SJUCapstone/BE/auth/dto/LoginResponse.java
+++ src/main/java/SJUCapstone/BE/auth/dto/LoginResponse.java
@@ -5,10 +5,8 @@ import lombok.Getter;
 @Getter
 public class LoginResponse {
     String refreshToken;
-    String accessToken;
 
-    public LoginResponse(String accessToken, String refreshToken) {
+    public LoginResponse(String refreshToken) {
         this.refreshToken = refreshToken;
-        this.accessToken = accessToken;
     }
 }
